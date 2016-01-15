bad4dc1955909733d11360836707ba79a8713b32
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-15 16:00:22 +0200

    fix Oauth

--- fixtures/default.js
+++ fixtures/default.js
@@ -62,11 +62,6 @@ module.exports = _.merge({
              * You should update this section with some test input for testing your module
              */
             input: {
-        //        name / Name
-        //    is_private / Private?
-        //description / Description
-        //scm / SCM
-                //Replace VAR1 with the name of an expected input, and add more inputs as needed.
                 owner: 'owner',
                 repo_slug: 'repo-slug',
                 is_private: 'true',
